0b8e1aaa3db5b71f293a9f41471b8408d08ed82d
ravi <ravi20930@gmail.com>
2024-04-27 23:47:10 +0530

    updated pdf processing script to be more accurate

--- packages/python/process.pdf.py
+++ packages/python/process.pdf.py
@@ -15,47 +15,57 @@ class PDFProcessor:
         self.csv_data = [["PDF File Name", "Text"]]
 
     def process(self):
+        print("Starting PDF processing")
         logging.info("Starting PDF processing")
         pdf_files = [file for file in os.listdir(self.folder_path) if file.endswith(".pdf")]
         for pdf_file in pdf_files:
             self.process_pdf(pdf_file)
         self.write_csv()
+        print("PDF processing completed")
         logging.info("PDF processing completed")
 
     def process_pdf(self, pdf_file):
+        print(f"Processing PDF: {pdf_file}")
         logging.info(f"Processing PDF: {pdf_file}")
         try:
             pdf_path = os.path.join(self.folder_path, pdf_file)
-            text, has_images, page_count = PDFTextExtractor.extract_text_and_images(pdf_path)
+            text, has_images, page_count, total_char_count, images = PDFTextExtractor.extract_text_and_images(pdf_path)
+            print(f"Pages iterated for {pdf_file}: {page_count}")
             logging.info(f"Pages iterated for {pdf_file}: {page_count}")
+            print(f"Images found in {pdf_file}: {has_images}")
             logging.info(f"Images found in {pdf_file}: {has_images}")
             if has_images:
+                print(f"Images found in {pdf_file}, processing images...")
                 logging.info(f"Images found in {pdf_file}, processing images...")
-                output_folder = os.path.join(self.folder_path, "images", pdf_file.split('.')[0])
-                os.makedirs(output_folder, exist_ok=True)
-                images = PDFImageConverter.convert_to_images(pdf_path, output_folder=output_folder)
-                image_texts = ImageProcessor.process_images(images)
-                text += ' '.join(image_texts)
+                for image_path in images:
+                    image_texts = ImageProcessor.process_images([image_path])
+                    text += ' '.join(image_texts)
             text = TextFilter.filter_text(text)
             self.csv_data.append([pdf_file, text])
         except Exception as e:
+            print(f"Error processing {pdf_file}: {e}")
             logging.error(f"Error processing {pdf_file}: {e}")
 
     def write_csv(self):
+        print("Writing CSV file")
         logging.info("Writing CSV file")
         with open(self.csv_file_path, "w", newline="", encoding="utf-8") as csv_file:
             writer = csv.writer(csv_file)
             writer.writerows(self.csv_data)
+        print(f"CSV file generated: {self.csv_file_path}")
         logging.info(f"CSV file generated: {self.csv_file_path}")
 
 
 class PDFTextExtractor:
     @staticmethod
     def extract_text_and_images(pdf_path):
+        print(f"Extracting text and images from PDF: {pdf_path}")
         logging.info(f"Extracting text and images from PDF: {pdf_path}")
         text = ""
         has_images = False
         page_count = 0
+        total_char_count = 0
+        images = []  # List to store paths of images to be extracted
         try:
             doc = fitz.open(pdf_path)
             for page_num, page in enumerate(doc):
@@ -63,47 +73,59 @@ class PDFTextExtractor:
                 page_text = page.get_text()
                 if page_text:
                     text += page_text
-                pix = page.get_pixmap()
-                if pix.n > 0:
-                    has_images = True
+                    total_char_count += len(page_text)
+                    print(f"Text count in page {page_num + 1}: {len(page_text)}")
+                    if total_char_count > 2000:  # Stop processing if total character count exceeds 2000
+                        print("Total character count exceeds 2000. Stopping processing.")
+                        logging.info("Total character count exceeds 2000. Stopping processing.")
+                        break
+                    if len(page_text) < 500:  # Check if the page has less than 500 characters of text
+                        pix = page.get_pixmap()
+                        if pix.n > 0:
+                            has_images = True
+                            print(f"Image found")
+                            logging.info(f"Image found")
+                            output_folder = os.path.join(os.path.dirname(pdf_path), "images", os.path.splitext(os.path.basename(pdf_path))[0])
+                            os.makedirs(output_folder, exist_ok=True)
+                            image_path = os.path.join(output_folder, f"page_{page_num + 1}.jpg")
+                            img = Image.frombytes("RGB", [pix.width, pix.height], pix.samples)
+                            img.save(image_path)
+                            images.append(image_path)
+                else:
+                    pix = page.get_pixmap()
+                    if pix.n > 0:
+                        has_images = True
+                        print(f"Image found")
+                        logging.info(f"Image found")
+                        output_folder = os.path.join(os.path.dirname(pdf_path), "images", os.path.splitext(os.path.basename(pdf_path))[0])
+                        os.makedirs(output_folder, exist_ok=True)
+                        image_path = os.path.join(output_folder, f"page_{page_num + 1}.jpg")
+                        img = Image.frombytes("RGB", [pix.width, pix.height], pix.samples)
+                        img.save(image_path)
+                        images.append(image_path)
         except Exception as e:
+            print(f"Error occurred while processing {pdf_path}: {e}")
             logging.error(f"Error occurred while processing {pdf_path}: {e}")
-        return text.strip(), has_images, page_count
-
-
-class PDFImageConverter:
-    @staticmethod
-    def convert_to_images(pdf_path, output_folder, dpi=150, quality=50):
-        logging.info(f"Converting PDF to images: {pdf_path}")
-        try:
-            doc = fitz.open(pdf_path)
-            images = []
-            for page_num, page in enumerate(doc):
-                image_path = os.path.join(output_folder, f"page_{page_num + 1}.jpg")
-                pix = page.get_pixmap(matrix=fitz.Identity, dpi=dpi,
-                                       colorspace=fitz.csRGB, clip=None, alpha=False, annots=True)
-                with Image.frombytes("RGB", [pix.width, pix.height], pix.samples) as img:
-                    img.save(image_path, "JPEG", quality=quality)
-                images.append(image_path)
-                logging.info(f"Page {page_num + 1} saved as image: {image_path}")
-            return images
-        except Exception as e:
-            logging.error(f"Error occurred while converting PDF to images: {e}")
+            return "", False, 0, 0, []  # Return empty text, False for has_images, and 0 for page_count and total_char_count in case of error
+        return text.strip(), has_images, page_count, total_char_count, images
 
 
 class ImageProcessor:
     @staticmethod
     def process_images(images):
+        print("Processing images")
         logging.info("Processing images")
         image_texts = []
         for image in images:
             try:
+                print(f"Processing image: {image}")
                 logging.info(f"Processing image: {image}")
                 reader = easyocr.Reader(['en'])
                 result = reader.readtext(image)
                 text = ' '.join([text[1] for text in result])
                 image_texts.append(text.strip())
             except Exception as e:
+                print(f"Error occurred while processing image {image}: {e}")
                 logging.error(f"Error occurred while processing image {image}: {e}")
         return image_texts
 
@@ -111,6 +133,7 @@ class ImageProcessor:
 class TextFilter:
     @staticmethod
     def filter_text(text):
+        print("Filtering text")
         logging.info("Filtering text")
         text = text.lower()
         pattern = r'[^a-z\s]'
